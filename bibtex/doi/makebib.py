5510cae3d34ffdb67e0f418a01cc249c99dd67cc
Jeff Candy <jcandy@san.rr.com>
2017-10-04 14:58:57 -0700

    Updates to makebib

--- bibtex/doi/makebib.py
+++ bibtex/doi/makebib.py
@@ -1,31 +1,36 @@
+# Usage: 
+#
+#    $ python makebib 
+#
+# Assumes doi.txt exists
+# Creates output.bib 
+
 import string
 import os
+import bibtexparser
+
+os.system('> output.bib')
 
 with open('doi.txt') as f:
     content = f.readlines()
 
 n = len(content)
 
-os.system('> candy.bib')
-
 for i in range(n):
    x = string.strip(content[i])
+   # Detect DOI prefix
    if (x[0:2] == "10"):
       os.system('bib get '+x+' > temp.bib')
-      os.system('cat temp.bib >> candy.bib')
-      with open('temp.bib') as f:
-         item = f.readlines()
-         for i in range(len(item)):
-            y = item[i].split("=")
-            if y[0].strip() == 'year':
-               year = y[1].translate(None,'{},\n')
-            if y[0].strip() == 'author':
-               author = y[1].translate(None,'{},\n')
-            if y[0].strip() == 'title':
-               title = y[1].translate(None,'{},\n')
-            if y[0].strip() == 'journal':
-               journal = y[1].translate(None,'{},\n')
-            if y[0].strip() == 'volume':
-               vol = y[1].translate(None,'{},\n')
-         print year,author
-               
+      os.system('cat temp.bib >> output.bib')
+      with open('temp.bib') as bibtex_file:
+         bibtex_str = bibtex_file.read()     
+      bib_database = bibtexparser.loads(bibtex_str)
+      bdict = bib_database.entries[0]
+      sauthor = bdict["author"].replace(" and",",")
+      # Human-readable screen output
+      print bdict["title"] +', '+sauthor
+      print bdict["journal"]+', '+bdict["volume"]+', '+bdict["year"]
+      print 'DOI: '+bdict["doi"]
+      print
+
+os.system('rm -f temp.bib')
